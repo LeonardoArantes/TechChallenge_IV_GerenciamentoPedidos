55d189c46672cb23de5130904d0306127f3105a3
LeonardoArantes <leonardoarantes86@gmail.com>
2024-04-09 17:37:51 -0300

    commit inicial

---
+++ src/main/java/com/fiap/pedido/SistemaDeGerenciamentoDePedidosIntegradoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoDePedidosIntegradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGerenciamentoDePedidosIntegradoApplication.class, args);
+	}
+
+}
